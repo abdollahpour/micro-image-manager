89525131dc34ed3c5a1767c1873d38d1df14c32f
Hamed Abdolahpour <hamed@abdollahpour.com>
2021-03-13 09:30:36 +0100

    Update tests

--- internal/server/server.go
+++ internal/server/server.go
@@ -17,13 +17,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func JSONError(w http.ResponseWriter, err interface{}, code int) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(err)
-}
-
 type StoreHandlerResult struct {
 	Id       string
 	Profiles []model.Profile
--- internal/storage/local_storage_test.go
+++ internal/storage/local_storage_test.go
@@ -28,6 +28,34 @@ func TestLocalStorageFileNotExist(t *testing.T) {
 	localStorage := NewLocalStorage(os.TempDir())
 
 	id := "279fc0dd-4160-4a10-ac43-4702477049ec"
-	_, err := localStorage.Fetch(id, model.Profile{Name: "large"}, "jpeg")
+	_, err := localStorage.Fetch(id, model.NewProfile("large"), "jpeg")
 	assert.NotNil(t, err)
+}
+func TestLocalStorgeStoreDefaultProfile(t *testing.T) {
+	localStorage := NewLocalStorage(os.TempDir())
+
+	id := "279fc0dd-4160-4a10-ac43-4702477049ec"
+
+	smallData := []byte{1, 2, 3}
+	err := localStorage.Store(id, model.DefaultProfile, "jpeg", smallData)
+	assert.Nil(t, err)
+	assertFetchedData(t, *localStorage, id, "small", smallData)
+
+	largeData := []byte{1, 2, 3, 4}
+	err = localStorage.Store(id, model.NewProfile("large"), "jpeg", largeData)
+	assert.Nil(t, err)
+	assertFetchedData(t, *localStorage, id, "large", largeData)
+
+	err = localStorage.Store(id, model.DefaultProfile, "jpeg", largeData)
+	assert.Nil(t, err)
+	assertFetchedData(t, *localStorage, id, "non_existing_profile", largeData)
+}
+
+func assertFetchedData(t *testing.T, localStorage LocalStorage, id string, profileName string, data []byte) {
+	fetched, err := localStorage.Fetch(id, model.NewProfile(profileName), "jpeg")
+	defer os.Remove(fetched)
+	assert.Nil(t, err)
+	fetchedData, err := ioutil.ReadFile(fetched)
+	assert.Nil(t, err)
+	assert.Equal(t, data, fetchedData)
 }
